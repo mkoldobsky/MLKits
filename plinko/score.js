3414e3c7a03972b58fd1920bd6c38f790f8eb496
Mariano Koldobsky <mariano.koldobsky@mercadolibre.com>
2019-03-14 13:28:53 -0300

    refactor accuracy

--- plinko/score.js
+++ plinko/score.js
@@ -10,14 +10,13 @@ function runAnalysis() {
   const testSetSize = 10;
   const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
   
-  let numberCorrect = 0;
-  for (let i = 0; i < testSet.length; i++) {
-    const bucket = knn(trainingSet, testSet[i][0]);
-    if (bucket === testSet[i][3]){
-      numberCorrect++;
-    }
-  }
-  console.log('Accuracy:', numberCorrect / testSetSize);
+  const accuracy = _.chain(testSet)
+    .filter(testPoint => knn(trainingSet, testPoint[0]) === testPoint[3])
+    .size()
+    .divide(testSetSize)
+    .value();
+
+  console.log('Accuracy:', accuracy);
 }
 
 
